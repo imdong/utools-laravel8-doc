d63803ebdf1cd11e003cf01c590f5b3fed3de694
ImDong <www@qs5.org>
2020-12-18 11:18:40 +0800

    初步完成基本功能

--- build.js
+++ build.js
@@ -1,40 +1,4 @@
-const fs = require('fs');
-const marked = require('marked');
-const hljs = require("highlight");
+const Bot = require('./lib/reptile_bot');
+const config = require('./config');
 
-fs.readFile('./data.html', (err, buffer) => {
-    let data = buffer.toString();
-
-    matchStyle(data);
-});
-
-fs.readFile('./md_docs/validation.md', (err, buffer) => {
-    let data = buffer.toString();
-
-    mdToHtml(data);
-});
-
-
-function matchStyle(html) {
-    let regex = '<link\\s*href="([^"]+)"';
-
-    html.match(new RegExp(regex, 'g')).map(item => {
-        let style_url = item.match(new RegExp(regex))[1],
-            style_name = style_url.split('/').pop();
-
-        console.log(style_url, style_name);
-
-        // 下载并保存至文件
-    })
-}
-
-function matchTranslator(html) {
-    // ^\s+<a\s*href="([^"]+)">[^<]+<img[^>]+title="([^"]+)"[^>]+>\s+<\/a>
-}
-
-function mdToHtml(md) {
-    let html = marked(md);
-    html = hljs.Highlight(html, false, true);
-
-    console.log(html)
-}
+(new Bot(config)).run();
